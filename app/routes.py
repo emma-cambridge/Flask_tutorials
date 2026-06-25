06be76f0dcae13b17ec48d445a92257d6a55f1e7
emma-cambridge <emma.cambridge1@education.nsw.gov.au>
2026-06-25 12:22:40 +1000

    chapter 4 pretty much done?
    I don't really understand the python shells and stuff but whatever hopefully its not important

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm
 
@@ -8,12 +8,12 @@ def index():
     user = {'username': 'Miguel'}
     posts = [
         {
-            'author': {'username': 'John'},
-            'body': 'Beautiful day in Portland!'
+            'author': {'username': 'Mia'},
+            'body': 'I like trains'
         },
         {
-            'author': {'username': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
+            'author': {'username': 'Dino'},
+            'body': 'RAHHHH!!!'
         }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
@@ -24,5 +24,5 @@ def login():
     if form.validate_on_submit():
         flash('Login requested for user {}, remember_me={}'.format(
             form.username.data, form.remember_me.data))
-        return redirect('/index')
+        return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
