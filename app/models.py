d33da4a56d6f118f3cf9c45933343c2338304862
emma-cambridge <emma.cambridge1@education.nsw.gov.au>
2026-07-02 13:30:33 +1000

    Still having problems with chapter 5

--- app/models.py
+++ app/models.py
@@ -5,6 +5,7 @@ import sqlalchemy.orm as so
 from app import db
 from app import login
 from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(UserMixin, db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
@@ -19,6 +20,11 @@ class User(UserMixin, db.Model):
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 class Post(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
